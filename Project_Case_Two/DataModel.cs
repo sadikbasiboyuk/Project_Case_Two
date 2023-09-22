eefeb2db0c578367038637fd08cb66c9a0837145
sadikbasiboyukaltamira <sadikbasiboyuk@altamira.com.tr>
2023-09-22 08:19:57 +0300

    initial commit

---
+++ Project_Case_Two/DataModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Project_Case_Two
+{
+    public class DataModel
+    {
+        public string locale { get; set; }
+        public string description { get; set; }
+        public Bounding boundingPoly { get; set; }
+    }
+
+    public class Bounding
+    {
+        public List<CoordinateModel> vertices { get; set; }
+    }
+
+    public class CoordinateModel
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
---
+++ Project_Case_Two/Program.cs
@@ -0,0 +1,47 @@
+﻿using Project_Case_Two;
+using System.Text;
+using System.Text.Json;
+
+string jsonContent = File.ReadAllText("response.json");
+var dataModelList = JsonSerializer.Deserialize<List<DataModel>>(jsonContent);
+var sb = new StringBuilder();
+
+// First element is ignored bec. of its locale
+var minY = dataModelList[1].boundingPoly.vertices.Min(k => k.y);
+var maxY = dataModelList[1].boundingPoly.vertices.Max(k => k.y);
+
+var billList = new List<List<DataModel>>
+{
+    new List<DataModel>() { dataModelList[1] }
+};
+
+for (int i = 2; i < dataModelList.Count; i++)
+{
+    bool added = false;
+    foreach (var bill in billList)
+    {
+        if (bill[0].boundingPoly.vertices.Average(x => x.y) > dataModelList[i].boundingPoly.vertices.Min(x => x.y)
+            && bill[0].boundingPoly.vertices.Average(x => x.y) < dataModelList[i].boundingPoly.vertices.Max(x => x.y)
+            )
+        {
+            bill.Add(dataModelList[i]);
+            added = true;
+            break;
+        }
+    }
+
+    if (!added)
+        billList.Add(new List<DataModel> { dataModelList[i] });
+
+    added = false;
+}
+
+foreach (var row in billList)
+{
+    foreach (var element in row)
+        sb.Append(element.description + ' ');
+
+    sb.Append('\n');
+}
+
+Console.WriteLine(sb.ToString());
